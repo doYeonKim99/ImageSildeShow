e0fddd2e8f90800d96baa8c9742b7cb407ebc6d7
Doyeon Kim <ehdus859599@gmail.com>
2016-05-26 20:53:04 +0900

    #2 chageTime

--- app/src/main/java/kr/hs/emirim/dok96/imagesileshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dok96/imagesileshow/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ViewFlipper flip;
+    EditText editTime;
+    int time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -15,9 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         flip= (ViewFlipper) findViewById(R.id.view_flip);
         flip.setFlipInterval(1000);
+        editTime=(EditText) findViewById (R.id.text_second);
         Button butStart=(Button)findViewById(R.id.but_start);
         butStart.setOnClickListener(this);
-        Button butstop = (Button) findViewById(R.id.but_stop);
+        Button butstop = (Button)findViewById(R.id.but_stop);
         butstop.setOnClickListener(this);
     }
 
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()) {
             case R.id.but_start:
+                time=(int)(Double.parseDouble(editTime.getText().toString())*1000);
+                flip.setFlipInterval((int) time);
                 flip.startFlipping();
                 break;
             case R.id.but_stop:
